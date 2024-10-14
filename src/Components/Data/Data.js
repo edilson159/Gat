13490d15c7638ac2a520e13ee0b6f3ead1b1678b
edilson159 <146896391+edilson159@users.noreply.github.com>
2024-10-14 19:39:35 -0300

    initial update

---
+++ src/Components/Data/Data.js
@@ -0,0 +1,42 @@
+const Data = [
+  {
+    Header: [
+      {
+        name: "../imgs/logo.svg",
+        icon: "../imgs/iconLogo.svg",
+      },
+      {
+        name: "podutos",
+        icon: "../imgs/iconMenu.svg",
+      },
+      {
+        name: "coleções",
+        icon: "../imgs/iconMenu.svg",
+      },
+      {
+        name: "blog",
+      },
+      {
+        name: "lookbook",
+      },
+      {
+        name: "sobre a Gat",
+      },
+      {
+        name: " wishlist",
+      },
+      {
+        placeholder: "digite aqui o que procura",
+        icon: "../imgs/iconLogo.svg",
+      },
+      {
+        icon: "../imgs/user.svg",
+      },
+      {
+        icon: "../imgs/iconLogo.svg",
+      },
+    ],
+  },
+];
+
+export default Data;
